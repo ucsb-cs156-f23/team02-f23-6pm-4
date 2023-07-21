ee4d4ae61d7fd2a53fd89a84b1ad5f56ebd7386d
Phill Conrad <phtcon@ucsb.edu>
2023-07-21 00:13:25 -0700

    pc - working version, we hope, for m23

--- src/main/java/edu/ucsb/cs156/example/config/SecurityConfig.java
+++ src/main/java/edu/ucsb/cs156/example/config/SecurityConfig.java
@@ -1,28 +1,35 @@
 package edu.ucsb.cs156.example.config;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import edu.ucsb.cs156.example.entities.User;
 import edu.ucsb.cs156.example.repositories.UserRepository;
@@ -30,18 +37,19 @@ import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true)
-public class SecurityConfig {
-
-  @Autowired
-  UserRepository userRepository;
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Slf4j
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Value("${app.admin.emails}")
   private final List<String> adminEmails = new ArrayList<String>();
 
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(authorize -> authorize
+  @Autowired
+  UserRepository userRepository;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests(authorize -> authorize
         .anyRequest().permitAll())
         .exceptionHandling(handlingConfigurer -> handlingConfigurer
             .authenticationEntryPoint(new Http403ForbiddenEntryPoint()))
@@ -52,8 +60,11 @@ public class SecurityConfig {
         .logout(logout -> logout
             .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
             .logoutSuccessUrl("/"));
+  }
 
-    return http.build();
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers("/h2-console/**");
   }
 
   private GrantedAuthoritiesMapper userAuthoritiesMapper() {
@@ -61,11 +72,13 @@ public class SecurityConfig {
       Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
 
       authorities.forEach(authority -> {
+        log.info("********** authority={}", authority);
         mappedAuthorities.add(authority);
         if (OAuth2UserAuthority.class.isInstance(authority)) {
           OAuth2UserAuthority oauth2UserAuthority = (OAuth2UserAuthority) authority;
 
           Map<String, Object> userAttributes = oauth2UserAuthority.getAttributes();
+          log.info("********** userAttributes={}", userAttributes);
 
           String email = (String) userAttributes.get("email");
           if (getAdmin(email)) {
@@ -89,11 +102,4 @@ public class SecurityConfig {
     Optional<User> u = userRepository.findByEmail(email);
     return u.isPresent() && u.get().getAdmin();
   }
-
-  @Bean
-  public WebSecurityCustomizer webSecurityCustomizer() {
-    return (web) -> web.ignoring()
-        .requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
-  }
-
 }
