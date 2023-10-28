7c9dd9b7a8c850c0ded0f4fb965d7577c587e97e
Sumit Gupta <sumitgupta@ucsb.edu>
2023-10-27 19:21:55 -0700

    sg - created entity class and repository class for RecommendationRequest database table

---
+++ src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
@@ -0,0 +1,30 @@
+package edu.ucsb.cs156.example.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "recommendationrequests")
+public class RecommendationRequest {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+  private String requesterEmail;
+  private String professorEmail;
+  private String explaination;
+  private LocalDateTime dateRequested;
+  private LocalDateTime dateNeeded;
+  private boolean done;
+}
