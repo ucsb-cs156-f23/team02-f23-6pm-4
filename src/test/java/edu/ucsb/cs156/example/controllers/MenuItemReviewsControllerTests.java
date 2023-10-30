3eadb137c933983c2d1cee43930b30633c645eca
Daryl Ou <darylou@ucsb.edu>
2023-10-30 12:03:15 -0700

    do - test name fixes

--- src/test/java/edu/ucsb/cs156/example/controllers/MenuItemReviewsControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/MenuItemReviewsControllerTests.java
@@ -144,11 +144,11 @@ public class MenuItemReviewsControllerTests extends ControllerTestCase{
             assertEquals(expectedJson, responseString);
     }
 
-    // Tests for DELETE /api/ucsbdates?id=... 
+    // Tests for DELETE /api/menuitemreviews?id=... 
 
         @WithMockUser(roles = { "ADMIN", "USER" })
         @Test
-        public void admin_can_delete_a_date() throws Exception {
+        public void admin_can_delete_a_review() throws Exception {
                 // arrange
 
                 LocalDateTime ldt1 = LocalDateTime.parse("2022-01-03T00:00:00");
@@ -179,7 +179,7 @@ public class MenuItemReviewsControllerTests extends ControllerTestCase{
         
         @WithMockUser(roles = { "ADMIN", "USER" })
         @Test
-        public void admin_tries_to_delete_non_existant_ucsbdate_and_gets_right_error_message()
+        public void admin_tries_to_delete_non_existant_menuitemreview_and_gets_right_error_message()
                         throws Exception {
                 // arrange
 
