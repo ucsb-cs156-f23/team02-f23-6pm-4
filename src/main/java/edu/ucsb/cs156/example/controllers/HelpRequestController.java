7ca5a3e7eb4b4ed460c426b2b0329c427bc8308c
Shuyi Wan <shuyiwan@ucsb.edu>
2023-10-30 23:30:55 -0700

    Shuyi added PUT method in controller of help request and done with the test

--- src/main/java/edu/ucsb/cs156/example/controllers/HelpRequestController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/HelpRequestController.java
@@ -86,4 +86,27 @@ public class HelpRequestController extends ApiController {
         return helpRequest;
     }
 
+
+    @Operation(summary= "Update a single help request")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("")
+    public HelpRequest updateHelpRequest(
+            @Parameter(name="id") @RequestParam Long id,
+            @RequestBody @Valid HelpRequest incoming) {
+
+        HelpRequest helpRequest = helpRequestRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(HelpRequest.class, id));
+
+        helpRequest.setRequesterEmail(incoming.getRequesterEmail());
+        helpRequest.setTeamId(incoming.getTeamId());
+        helpRequest.setTableOrBreakoutRoom(incoming.getTableOrBreakoutRoom());
+        helpRequest.setRequestTime(incoming.getRequestTime());
+        helpRequest.setExplanation(incoming.getExplanation());
+        helpRequest.setSolved(incoming.getSolved());
+
+        helpRequestRepository.save(helpRequest);
+
+        return helpRequest;
+    }
+
 }
