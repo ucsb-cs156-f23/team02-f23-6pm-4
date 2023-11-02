72e403b4e614ddfdc5bb24d5912be0cdbbfdd72a
David Wang <david.wang5766@gmail.com>
2023-11-02 00:34:44 -0700

    Added a PUT endpoint and associated tests for Articles

--- src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
@@ -83,4 +83,23 @@ public class ArticlesController extends ApiController {
         return article;
     }
 
+    @Operation(summary = "Update a single article")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("")
+    public Articles updateArticles(
+            @Parameter(name = "id") @RequestParam Long id,
+            @RequestBody @Valid Articles incoming) {
+        Articles article = articlesRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Articles.class, id));
+        article.setTitle(incoming.getTitle());
+        article.setUrl(incoming.getUrl());
+        article.setExplanation(incoming.getExplanation());
+        article.setEmail(incoming.getEmail());
+        article.setDateAdded(incoming.getDateAdded());
+
+        articlesRepository.save(article);
+
+        return article;
+    }
+
 }
